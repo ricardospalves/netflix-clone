52e0a958ff6f134ca60723b3d25056bfe1d0ff76
ricardospalves <ricardospalves@gmail.com>
2022-03-17 22:28:38 -0300

    add cors and types

--- webservice/src/app.ts
+++ webservice/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import './services/database'
 
 import { router } from './routes'
@@ -8,6 +9,7 @@ const PORT = 4000
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
